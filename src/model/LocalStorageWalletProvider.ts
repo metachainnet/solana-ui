90053e1cce5ca62ec1b9bfa10b60f21b3e13ee1c
BiFoS <jon89071@gmail.com>
2022-03-17 19:20:47 +0900

    merge wallet

--- src/model/LocalStorageWalletProvider.ts
+++ src/model/LocalStorageWalletProvider.ts
@@ -1,21 +1,66 @@
-import { Keypair, Transaction } from "@solana/web3.js";
-import base58 from "bs58";
+import { Account } from "@solana/web3.js";
+import { BIP32Factory } from "bip32";
+import { derivePath } from "ed25519-hd-key";
+import * as ecc from "tiny-secp256k1";
 import nacl from "tweetnacl";
-import { getUnlockedMnemonicAndSeed } from "../lib/wallet-seed";
-import { getAccountFromSeed, WalletArgs } from "./Wallet";
+import { getUnlockedMnemonicAndSeed } from "../utils/wallet-seed";
 
-export default class LocalStorageWalletProvider {
-  account: Keypair;
+export const DERIVATION_PATH = {
+  deprecated: undefined,
+  bip44: "bip44",
+  bip44Change: "bip44Change",
+  bip44Root: "bip44Root", // Ledger only.
+};
 
-  constructor(args: WalletArgs) {
+export function getAccountFromSeed(
+  seed: Buffer,
+  walletIndex: number,
+  dPath: string | undefined | null = undefined,
+  accountIndex = 0
+) {
+  const derivedSeed = deriveSeed(seed, walletIndex, dPath, accountIndex);
+  return new Account(
+    nacl.sign.keyPair.fromSeed(derivedSeed as Buffer).secretKey
+  );
+}
+
+function deriveSeed(
+  seed: string | Buffer,
+  walletIndex: number,
+  derivationPath: string | null | undefined,
+  accountIndex: number
+) {
+  switch (derivationPath) {
+    case DERIVATION_PATH.deprecated:
+      const path = `m/501'/${walletIndex}'/0/${accountIndex}`;
+      return BIP32Factory(ecc)
+        .fromSeed(seed as Buffer)
+        .derivePath(path).privateKey;
+    case DERIVATION_PATH.bip44:
+      const path44 = `m/44'/501'/${walletIndex}'`;
+      return derivePath(path44, seed as string).key;
+    case DERIVATION_PATH.bip44Change:
+      const path44Change = `m/44'/501'/${walletIndex}'/0'`;
+      return derivePath(path44Change, seed as string).key;
+    default:
+      throw new Error(`invalid derivation path: ${derivationPath}`);
+  }
+}
+
+export class LocalStorageWalletProvider {
+  publicKey: any;
+  listAddresses: any;
+  account: any;
+  constructor(args: any) {
     this.account = args.account;
+    this.publicKey = this.account.publicKey;
   }
 
   init = async () => {
     const { seed } = await getUnlockedMnemonicAndSeed();
-    const addressList = async (walletCount: number) => {
+    this.listAddresses = async (walletCount: number) => {
       const seedBuffer = Buffer.from(seed, "hex");
-      return [...Array(walletCount).keys()].map((walletIndex: number) => {
+      return [...Array(walletCount).keys()].map((walletIndex) => {
         let address = getAccountFromSeed(seedBuffer, walletIndex).publicKey;
         let name = localStorage.getItem(`name${walletIndex}`);
         return { index: walletIndex, address, name };
@@ -24,12 +69,12 @@ export default class LocalStorageWalletProvider {
     return this;
   };
 
-  signTransaction = async (transaction: Transaction) => {
-    transaction.partialSign(this.account);
-    return transaction;
-  };
+  //   signTransaction = async (transaction) => {
+  //     transaction.partialSign(this.account);
+  //     return transaction;
+  //   };
 
-  createSignature = (message: Uint8Array) => {
-    return base58.encode(nacl.sign.detached(message, this.account.secretKey));
-  };
+  //   createSignature = (message) => {
+  //     return bs58.encode(nacl.sign.detached(message, this.account.secretKey));
+  //   };
 }
--- src/model/Wallet.ts
+++ src/model/Wallet.ts
@@ -4,34 +4,112 @@ import { derivePath } from "ed25519-hd-key";
 import * as ecc from "tiny-secp256k1";
 import nacl from "tweetnacl";
 import { DERIVATION_PATH } from "../lib/wallet-seed";
-import LedgerWalletProvider from "./LedgerWalletProvider";
-import LocalStorageWalletProvider from "./LocalStorageWalletProvider";
-import WalletProviderFactory from "./WalletProviderFactory";
+import { LocalStorageWalletProvider } from "./LocalStorageWalletProvider";
 
-export default class Wallet {
+export class Wallet {
   connection: Connection;
-  type: "local" | "ledger";
-  provider: LedgerWalletProvider | LocalStorageWalletProvider; // todo : 인터페이스로 추상화 필요
+  provider: LocalStorageWalletProvider;
 
-  private constructor(
-    connection: Connection,
-    type: "local" | "ledger",
-    args: WalletArgs
-  ) {
-    this.connection = connection;
-    this.type = type;
-    this.provider = WalletProviderFactory.getProvider(type, args);
+  constructor(_connection: Connection, args: any) {
+    this.connection = _connection;
+    this.provider = new LocalStorageWalletProvider(args);
   }
 
-  static create = async (
-    connection: Connection,
-    type: "local" | "ledger",
-    args: any
-  ) => {
-    const instance = new Wallet(connection, type, args);
+  static create = async (connection: Connection, args: any) => {
+    const instance = new Wallet(connection, args);
     await instance.provider.init();
     return instance;
   };
+
+  get publicKey() {
+    return this.provider.publicKey;
+  }
+
+  // getTokenAccountInfo = async () => {
+  //   let accounts = await getOwnedTokenAccounts(this.connection, this.publicKey);
+  //   return accounts
+  //     .map(({ publicKey, accountInfo }) => {
+  //       setInitialAccountInfo(this.connection, publicKey, accountInfo);
+  //       return { publicKey, parsed: parseTokenAccountData(accountInfo.data) };
+  //     })
+  //     .sort((account1, account2) =>
+  //       account1.parsed.mint
+  //         .toBase58()
+  //         .localeCompare(account2.parsed.mint.toBase58())
+  //     );
+  // };
+
+  // createTokenAccount = async (tokenAddress) => {
+  //   return await createAndInitializeTokenAccount({
+  //     connection: this.connection,
+  //     payer: this,
+  //     mintPublicKey: tokenAddress,
+  //     newAccount: new Account(),
+  //   });
+  // };
+
+  // createAssociatedTokenAccount = async (splTokenMintAddress) => {
+  //   return await createAssociatedTokenAccount({
+  //     connection: this.connection,
+  //     wallet: this,
+  //     splTokenMintAddress,
+  //   });
+  // };
+
+  // tokenAccountCost = async () => {
+  //   return this.connection.getMinimumBalanceForRentExemption(
+  //     ACCOUNT_LAYOUT.span
+  //   );
+  // };
+
+  // transferToken = async (
+  //   source,
+  //   destination,
+  //   amount,
+  //   mint,
+  //   decimals,
+  //   memo = null,
+  //   overrideDestinationCheck = false
+  // ) => {
+  //   if (source.equals(this.publicKey)) {
+  //     if (memo) {
+  //       throw new Error("Memo not implemented");
+  //     }
+  //     return this.transferSol(destination, amount);
+  //   }
+  //   return await transferTokens({
+  //     connection: this.connection,
+  //     owner: this,
+  //     sourcePublicKey: source,
+  //     destinationPublicKey: destination,
+  //     amount,
+  //     memo,
+  //     mint,
+  //     decimals,
+  //     overrideDestinationCheck,
+  //   });
+  // };
+
+  // transferSol = async (destination, amount) => {
+  //   return nativeTransfer(this.connection, this, destination, amount);
+  // };
+
+  // closeTokenAccount = async (publicKey, skipPreflight = false) => {
+  //   return await closeTokenAccount({
+  //     connection: this.connection,
+  //     owner: this,
+  //     sourcePublicKey: publicKey,
+  //     skipPreflight,
+  //   });
+  // };
+
+  // signTransaction = async (transaction) => {
+  //   return this.provider.signTransaction(transaction);
+  // };
+
+  // createSignature = async (message) => {
+  //   return this.provider.createSignature(message);
+  // };
 }
 
 export interface WalletArgs {
