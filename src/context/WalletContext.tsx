592c6a8733704e6ca42948583db9147e37089d44
BiFoS <jon89071@gmail.com>
2022-03-17 19:21:07 +0900

    merge wallet

--- src/context/WalletContext.tsx
+++ src/context/WalletContext.tsx
@@ -1,10 +1,7 @@
-import { Connection } from "@solana/web3.js";
 import React, { useContext, useEffect, useMemo, useState } from "react";
 import { useLocalStorageState } from "../hooks/useLocalStorageState";
-import {
-  getAccountFromSeed,
-  LocalStorageWalletProvider,
-} from "../LocalStorageWalletProvider";
+import { getAccountFromSeed } from "../model/LocalStorageWalletProvider";
+import { Wallet } from "../model/Wallet";
 import { useUnlockedMnemonicAndSeed } from "../utils/wallet-seed";
 import { useConnection } from "./ConnectionContext";
 
@@ -13,112 +10,6 @@ const DEFAULT_WALLET_SELECTOR = {
   importedPubkey: undefined,
   ledger: false,
 };
-
-export class Wallet {
-  connection: Connection;
-  provider: LocalStorageWalletProvider;
-  constructor(_connection: Connection, args: any) {
-    this.connection = _connection;
-    this.provider = new LocalStorageWalletProvider(args);
-  }
-
-  static create = async (connection: Connection, args: any) => {
-    const instance = new Wallet(connection, args);
-    await instance.provider.init();
-    return instance;
-  };
-
-  get publicKey() {
-    return this.provider.publicKey;
-  }
-
-  // getTokenAccountInfo = async () => {
-  //   let accounts = await getOwnedTokenAccounts(this.connection, this.publicKey);
-  //   return accounts
-  //     .map(({ publicKey, accountInfo }) => {
-  //       setInitialAccountInfo(this.connection, publicKey, accountInfo);
-  //       return { publicKey, parsed: parseTokenAccountData(accountInfo.data) };
-  //     })
-  //     .sort((account1, account2) =>
-  //       account1.parsed.mint
-  //         .toBase58()
-  //         .localeCompare(account2.parsed.mint.toBase58())
-  //     );
-  // };
-
-  // createTokenAccount = async (tokenAddress) => {
-  //   return await createAndInitializeTokenAccount({
-  //     connection: this.connection,
-  //     payer: this,
-  //     mintPublicKey: tokenAddress,
-  //     newAccount: new Account(),
-  //   });
-  // };
-
-  // createAssociatedTokenAccount = async (splTokenMintAddress) => {
-  //   return await createAssociatedTokenAccount({
-  //     connection: this.connection,
-  //     wallet: this,
-  //     splTokenMintAddress,
-  //   });
-  // };
-
-  // tokenAccountCost = async () => {
-  //   return this.connection.getMinimumBalanceForRentExemption(
-  //     ACCOUNT_LAYOUT.span
-  //   );
-  // };
-
-  // transferToken = async (
-  //   source,
-  //   destination,
-  //   amount,
-  //   mint,
-  //   decimals,
-  //   memo = null,
-  //   overrideDestinationCheck = false
-  // ) => {
-  //   if (source.equals(this.publicKey)) {
-  //     if (memo) {
-  //       throw new Error("Memo not implemented");
-  //     }
-  //     return this.transferSol(destination, amount);
-  //   }
-  //   return await transferTokens({
-  //     connection: this.connection,
-  //     owner: this,
-  //     sourcePublicKey: source,
-  //     destinationPublicKey: destination,
-  //     amount,
-  //     memo,
-  //     mint,
-  //     decimals,
-  //     overrideDestinationCheck,
-  //   });
-  // };
-
-  // transferSol = async (destination, amount) => {
-  //   return nativeTransfer(this.connection, this, destination, amount);
-  // };
-
-  // closeTokenAccount = async (publicKey, skipPreflight = false) => {
-  //   return await closeTokenAccount({
-  //     connection: this.connection,
-  //     owner: this,
-  //     sourcePublicKey: publicKey,
-  //     skipPreflight,
-  //   });
-  // };
-
-  // signTransaction = async (transaction) => {
-  //   return this.provider.signTransaction(transaction);
-  // };
-
-  // createSignature = async (message) => {
-  //   return this.provider.createSignature(message);
-  // };
-}
-
 interface IWalletContext {
   wallet: any;
   seed?: any;
@@ -264,6 +155,27 @@ export const WalletProvider: React.FC<React.ReactNode> = ({ children }) => {
     </WalletContext.Provider>
   );
 };
-export function useWallet() {
+export function useWallet(): Wallet {
   return useContext(WalletContext).wallet;
+}
+
+export function useWalletSelector() {
+  const {
+    accounts,
+    derivedAccounts,
+    addAccount,
+    setWalletSelector,
+    setAccountName,
+    hardwareWalletAccount,
+    setHardwareWalletAccount,
+  } = useContext(WalletContext);
+
+  return {
+    accounts,
+    derivedAccounts,
+    addAccount,
+    setAccountName,
+    hardwareWalletAccount,
+    setHardwareWalletAccount,
+  };
 }
