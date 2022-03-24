5e5d1e44c2c4df5c4fecb0271a72503df94daed9
hk-the-future <hokyung.thefuture@gmail.com>
2022-03-24 21:26:30 +0900

    :fire: delete project
    delete project

--- src/lib/utils.ts
+++
@@ -1,6 +0,0 @@
-import { PublicKey } from "@solana/web3.js";
-
-export function abbreviateAddress(address: PublicKey) {
-  const base58 = address.toBase58();
-  return `${base58.slice(0, 4)}…${base58.slice(base58.length - 4)}`;
-}
